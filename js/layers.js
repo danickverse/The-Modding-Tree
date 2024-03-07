4d0f70536fb3d9253bedfcd1b4c9d6999d5d4a94
nicholas-parra <nick.parra555@gmail.com>
2024-03-07 12:30:55 -0800

    Second sesh

--- js/layers.js
+++ js/layers.js
@@ -5,6 +5,7 @@ addLayer("p", {
     startData() { return {
         unlocked: true,
 		points: new Decimal(0),
+        best: new Decimal(0)
     }},
     color: "#AD6F69",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
@@ -39,26 +40,92 @@ addLayer("e", {
     position: 1,
     startData() { return {
         unlocked: true,
-        points: new Decimal(0)
+        points: new Decimal(0),
+        penny_expansions: {
+             points: new Decimal(0)
+        }
     }},
     color: "#FFFFFF",
     resource: "expansions",
     baseResource: "points",
-    type: "none",
-    passiveGeneration() {
-        return Math.log10(Math.log10(10 + Math.max(1,player.points))) / 1000
-    },
     baseAmount() {return player.points},
-    exponent: .5,
-    gainMult() {
-        return new Decimal(1)
+    type: "custom",
+    getResetGain() {
+        if (player.points.lessThan(new Decimal("1e10"))) return new Decimal("0")
+        return new Decimal((Math.log10(Math.log10(10 + player.points)) - 1) / 10)
     },
-    gainExp() {
-        return new Decimal(1)
+    prestigeButtonText(){
+        return "hello"
     },
+    getNextAt() {return new Decimal(0)},
+    baseAmount() {return player.points},
+    doReset(layer) {},
     row: 0,
     layerShown() {return true},
     update(diff) {
-        player[this.layer].points = player[this.layer].points.add(1)
+        let layerData = player[this.layer]
+        let penny_expansions = layerData.penny_expansions
+        layerData.points = layerData.points.add(getResetGain(this.layer).times(diff))
+        penny_expansions.points = penny_expansions.points.add(tmp.e.penny_expansions.getResetGain.times(diff))
+    },
+    canReset() {return false},
+    penny_expansions: {
+        getResetGain() {
+            if (player.e.points.lessThan(new Decimal("1"))) return new Decimal("0")
+            return new Decimal(Math.log10(9 + player.e.points) / 100)
+        },
+        getGainMult() {
+            return new Decimal("1")
+        }
+    },
+    tabFormat: {
+        "Info": {
+            content: [
+                "main-display",
+                ["display-text",
+                    function() {
+                        if (player.shiftAlias) return "You are gaining max(0, log10(log10(10 + points)) - 1) / 1000 Expansions per second"
+                        return "You are gaining " + format(tmp.e.getResetGain) + " Expansions per second"
+                    }
+                ],
+                ["display-text",
+                    function() {
+                        return "You begin gaining expansions when your points surpass a value of 1e10"
+                    }
+                ]
+            ]
+        },
+        "Pennies": {
+            content: [
+                "main-display",
+                ["display-text",
+                    function(){
+                        return "You have " + format(player.e.penny_expansions.points) + " Penny Expansions"
+                    }
+                ],
+                ["display-text",
+                    function(){
+                        if (player.shiftAlias) return "You are gaining log10(9 + expansions) / 100 Penny Expansions per second"
+                        return "You are gaining " + format(tmp.e.penny_expansions.getResetGain) + " Penny Expansions per second"
+                    }
+                ],
+                "blank",
+                ["upgrades", [1]], 
+                "blank",
+            ],
+            unlocked(){
+                return true
+            },
+        }
+    },
+    upgrades: {
+        11: {
+            title: "The first expansion one.",
+            description: "Multiplies expansion gain by 2.",
+            cost: new Decimal("1"),
+            currencyDisplayName:() => "Penny Expansions",
+            currencyInternalName:() => "points",
+            currencyLocation:() => player.e.penny_expansions
+        }
     }
 })
